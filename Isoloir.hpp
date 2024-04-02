01cb7c7e2af83bd48e816f6ad53579f07ff048e6
Bertrand RIVARD <bertrand.rivard@etu.univ-nantes.fr>
2024-04-02 16:58:49 +0200

    utilisation de la queue au lieu de FIleAttente

--- Isoloir.hpp
+++ Isoloir.hpp
@@ -5,7 +5,7 @@
 
 #include <string>
 #include "Espace.hpp"
-#include "FileAttenteCapacite.hpp"
+#include <queue>
 
 #ifndef ISOLOIR_HPP
 #define ISOLOIR_HPP
@@ -16,7 +16,7 @@ class Isoloir {
     Isoloir(const int Di, const std::string& nom, int nbIsoloir); /*! Constructeur*/
     ~Isoloir(); /*! Destructeur */
     bool estVide(); /*! Vérifie si l'isoloir est vide ou non */
-    void ajouterPersonne(Personne* persDecharge); /*! Ajoute une personne à l'espace Isoloir */
+    void ajouterPersonne(Personne* pers); /*! Ajoute une personne à l'espace Isoloir */
     Personne* sortirPersonne(); /*! Sort une personne de l'isoloir et renvoie cette personne */
 
     private :
@@ -24,8 +24,8 @@ class Isoloir {
     std::string nom; /*! Nom de l'isoloir */
     int Di; /*Durée dans lequel une personne reste dans l'isoloir */
     int nbIsoloir; /*Nombre d'isoloir différents dans l'esapce Isoloir*/
-    FileAttenteCapacite persDansIsoloir; /* Comme il y a plusieurs personnes dans différents isoloir, on utilise une fileAttenteCapacite*/
-    FileAttente fileIsoloir; /* Soit la file des personnes qui attendent qu'un isoloir se libère*/
+    std::queue<Personne*> persDansIsoloir; /* Comme il y a plusieurs personnes dans différents isoloir, on utilise une fileAttenteCapacite*/
+    std::queue<Personne*> fileIsoloir; /* Soit la file des personnes qui attendent qu'un isoloir se libère*/
 };
 
 #endif //ISLOIR_HPP

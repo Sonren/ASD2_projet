331a881738857e762f53d10c95c85dd01dfec461
Bertrand Rivard <bertrandrivard@etu.univ-nantes.fr>
2024-04-04 15:18:55 +0200

    Ajout commmentaire Electeur.hpp

--- Electeur.hpp
+++ Electeur.hpp
@@ -15,18 +15,30 @@ class Electeur : public elections::Personne {
 
     public:
 
-    Electeur(const std::string& nom, const std::string& prenom, int spol,int duree);
+
+    // Déclaration du constructeur de la classe Electeur avec les paramètres nom, prénom, spol (probablement signifiant le sexe politique) et duree (durée)
+    Electeur(const std::string& nom, const std::string& prenom, int spol, int duree);
+    
+    // Déclaration de la méthode getDuree() pour obtenir la durée
     int getDuree();
-    void steDuree(int d);
+    
+    // Déclaration de la méthode setDuree() pour définir la durée
+    void setDuree(int d);
+    
+    // Déclaration de la méthode getChoix() pour obtenir le choix de vote de l'électeur
     elections::Personne* getChoix();
+    
+    // Déclaration de la méthode setChoix() pour définir le choix de vote de l'électeur
     void setChoix(elections::Personne* p);
+    
+    // Déclaration de la méthode ajouterBulletin() pour ajouter un bulletin de vote
     void ajouterBulletin(Bulletin* bult);
 
     private:
 
-    std::vector<Bulletin*> liste_bulletin;
-    int duree_espace;
-    elections::Personne* choix_vote;
+    std::vector<Bulletin*> liste_bulletin; //la liste des bulletins qu'il prends dans la table de décharge
+    int duree_espace; //Durée où l'élécteur doit rester dans l'espace
+    elections::Personne* choix_vote; //Pointe vers le candidat choisis 
 
 };
 #endif
